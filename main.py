3ff6d42d13349251cf69c9c6c90502d3533eb047
Vinayaka Patrick Thompson <vinayaka.p.thompson@live.com>
2021-05-03 23:44:32 -0700

    Created basic file io.

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 def BulkDataGenerator():
-  print("provide mapping file")
-  print("provide data file")
-  print("provide output file")
+  print("")
+  mappingFileName = input("Provide mapping file: ")
+  dataFileName = input("Provide data file: ")
+  outputFileName = input("Provide output file: ")
+   
+  with open(mappingFileName, 'r') as mappingFile:
+    firstline = mappingFile.readline()
+
+
   print("reformating Data:")
+
+
+
+  with open(outputFileName, 'w') as outputFile:
+    data = 'Test data to output file'
+    outputFile.write(data)
+    outputFile.write(firstline)
+
   print("done!")
+
 BulkDataGenerator()
