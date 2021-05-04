25f27aed3730221ceb81e8647b64a8a39933b613
Vinayaka Patrick Thompson <vinayaka.p.thompson@live.com>
2021-05-03 23:29:20 -0700

    made basic prints to show whats going on.

---
+++ main.py
@@ -0,0 +1,7 @@
+def BulkDataGenerator():
+  print("provide mapping file")
+  print("provide data file")
+  print("provide output file")
+  print("reformating Data:")
+  print("done!")
+BulkDataGenerator()
