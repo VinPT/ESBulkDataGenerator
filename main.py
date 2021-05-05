cf617f6eb327577b67ca62d477e059e713e9f859
Vinayaka Patrick Thompson <vinayaka.p.thompson@live.com>
2021-05-05 00:08:08 -0700

    Made system for making sure file provided is valid

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 def BulkDataGenerator():
-  print("")
-  mappingFileName = input("Provide mapping file: ")
-  dataFileName = input("Provide data file: ")
-  outputFileName = input("Provide output file: ")
-   
-  with open(mappingFileName, 'r') as mappingFile:
-    firstline = mappingFile.readline()
+    mappingFileName = input("Provide mapping file: ")
+    outputFileName = input("Provide output file: ")
+
+    with open(mappingFileName, 'r') as mappingFile:
+        for line in mappingFile:
+            print("This is not something that workes yet.")
+            firstline = line
+
+    with openDataFile("Provide data file: ") as dataFile:
+        for line in dataFile:
+            print(line)
+        print("This is not something that workes yet.")
+
+    print("reformating Data:")
+
+    with open(outputFileName, 'w') as outputFile:
+        data = 'Test data to output file'
+        outputFile.write(data)
+        outputFile.write(firstline)
+
+    print("done!")
 
 
-  print("reformating Data:")
+def openDataFile(requestText):
+    while True:
+        fileName = input(requestText)
 
+        try:
+            file = open(fileName, 'r')
+            break
+        except:
+            print("Invalid file provided")
+    return file
 
-
-  with open(outputFileName, 'w') as outputFile:
-    data = 'Test data to output file'
-    outputFile.write(data)
-    outputFile.write(firstline)
-
-  print("done!")
 
 BulkDataGenerator()
